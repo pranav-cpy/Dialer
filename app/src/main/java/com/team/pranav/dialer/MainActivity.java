69ed55afa7666c17e69d9893db133784eb088eed
pranav <pranavcpy10@gmail.com>
2018-03-17 04:45:34 +0530

    commit

--- app/src/main/java/com/team/pranav/dialer/MainActivity.java
+++ app/src/main/java/com/team/pranav/dialer/MainActivity.java
@@ -2,11 +2,16 @@ package com.team.pranav.dialer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,call;
+    TextView name,provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        b0=(Button)findViewById(R.id.no0);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
