2409141b8e18138a554ea6617b964ff14876e3f2
pranav <pranavcpy10@gmail.com>
2018-03-17 08:00:36 +0530

    commit

--- app/src/main/java/com/team/pranav/dialer/MainActivity.java
+++ app/src/main/java/com/team/pranav/dialer/MainActivity.java
@@ -20,17 +20,28 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Locale;
 
 import static android.R.attr.author;
 import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
 
 public class MainActivity extends AppCompatActivity {
-    Button b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,call,change;
+
+
+
+    HttpClient
+    String
+
+
+    Button b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,change;
+    ImageView back;
     TextView number;
     String str="",string;
     ImageButton imgbtn;
     TextToSpeech tts;
-    int result;
+    int result,no=0;
     public static final String prefs = "myprefsfile";
     String[] name1={"Mom","Anjali","Sidhin","Hadeeb","Bineeth","Arathy"};
     String[] provider1={"jio","idea","idea","jio","jio","idea"};
@@ -46,8 +57,11 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         b0=(Button)findViewById(R.id.no0);
         b1=(Button)findViewById(R.id.no1);
         b2=(Button)findViewById(R.id.no2);
@@ -60,85 +74,92 @@ public class MainActivity extends AppCompatActivity {
         b9=(Button)findViewById(R.id.no9);
         imgbtn=(ImageButton)findViewById(R.id.call);
         number=(TextView)findViewById(R.id.number_display);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        change=(Button)findViewById(R.id.change);
+        back=(ImageView)findViewById(R.id.back);
 
         SharedPreferences settings = getSharedPreferences(MainActivity.prefs, 0);
         SharedPreferences.Editor editor = settings.edit();
         editor.putString("number","8606851382");
         editor.apply();
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                no=no/10;
+                number.setText(no);
+            }
+        });
 
         b0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                str+="0";
-                number.setText(str);
+                no=0;
+                number.setText(no);
             }
         });
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                str+="1";
-                number.setText(str);
+                no=no*10+1;
+                number.setText(no);
             }
         });
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                str+="2";
-                number.setText(str);
+                no=no*10+2;
+                number.setText(no);
             }
         });
         b3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                str+="3";
-                number.setText(str);
+                no=no*10+3;
+                number.setText(no);
             }
         });
         b4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                str+="4";
-                number.setText(str);
+                no=no*10+4;
+                number.setText(no);
             }
         });
         b5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                str+="5";
-                number.setText(str);
+                no=no*10+5;
+                number.setText(no);
             }
         });
         b6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                str+="6";
-                number.setText(str);
+                no=no*10+6;
+                number.setText(no);
             }
         });
         b7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                str+="7";
-                number.setText(str);
+                no=no*10+7;
+                number.setText(no);
             }
         });
         b8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                str+="8";
-                number.setText(str);
+                no=no*10+8;
+                number.setText(no);
             }
         });
         b9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                str+="9";
-                number.setText(str);
+                no=no*10+9;
+                number.setText(no);
             }
         });
-        call.setOnClickListener(new View.OnClickListener() {
+        imgbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 voiceoutput();
@@ -161,6 +182,18 @@ public class MainActivity extends AppCompatActivity {
         ListView listview = (ListView) findViewById(R.id.listview1);
         CustomAdapter customadapter = new CustomAdapter(this);
         listview.setAdapter(customadapter);
+
+        tts = new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    result = tts.setLanguage(Locale.UK);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Feature not supported in your device", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
 
 
     }
@@ -198,6 +231,34 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void voiceoutput(){
+        if(result==TextToSpeech.LANG_NOT_SUPPORTED||result==TextToSpeech.LANG_MISSING_DATA) {
+            Toast.makeText(getApplicationContext(),"Feature not supported in your device",Toast.LENGTH_SHORT).show();
+        }else {
+            tts.speak("Welcome to blood donor finder. Press one for request, presss two for blood donation, press 3 for giving feedback followed by #", TextToSpeech.QUEUE_FLUSH,null);
+            input(mainoption);
+            if(mainoption==1){
+                tts.speak("Press 1 for A positive, press 2 for A negative, press 3 for b positive, press 4 for b negative, press 5 for o positive, press 6 for o negative followed by #", TextToSpeech.QUEUE_FLUSH,null);
+                input(group);
+                tts.speak("press 1 for trivandrum, press 2 for kollam , press 3 for pathanamthitta, press 4 for alappuzha, press 5 for kottayam, press 6 idukki, press 7 for ernamkulam, press 8 for thrissur, press 9 for palakkad, press 10 for malappuram, press 11 for kozhikkod, press 12 for wayanad, press 13 for kannur, press 14 for kasargod followed by # ", TextToSpeech.QUEUE_FLUSH,null);
+                input(location);
+                tts.speak("Press 1 for low volume, press 2 for high volume followed by #", TextToSpeech.QUEUE_FLUSH,null);
+                input(volume);
+                JSONObject json=new JSONObject();
+                try {
+                    json.put("label","request");
+                    json.put("group",group);
+                    json.put("location",location);
+                    json.put("volume",volume);
+                    JSONObject j = HttpClient.SendHttpPost(URL,json);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            else if(mainoption==2){
+                tts.speak(" followed by #", TextToSpeech.QUEUE_FLUSH,null);
+            }
+
+        }
 
 
     }
